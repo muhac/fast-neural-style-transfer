6da4184901250c44b82be7a2cb508f3e7136869c
Muhan Li <limuhan@msn.com>
2020-06-26 03:25:49 +0800

    微信小程序更新
    查看大图和下载图片

--- NST/WeChatMiniProgram/pages/index/index.js
+++ NST/WeChatMiniProgram/pages/index/index.js
@@ -8,12 +8,13 @@ Page({
     imgFile: '',
     generate: '',
     block: 'none',
+    showimg: 'none',
     style: 1,
     style_pos: [220, 5],
   },
 
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -40,14 +41,14 @@ Page({
           imgFile: res.tempFilePaths[0],
           img: base64,
           generate: ''
-        }) 
+        })
       }
     })
   },
 
   chooseStyle: function (e) {
     var id = e.currentTarget.dataset['index']
-    var y = parseInt((id-1) / 3)
+    var y = parseInt((id - 1) / 3)
     var x = parseInt(id - 1 - y * 3)
     this.setData({
       style: id,
@@ -89,26 +90,35 @@ Page({
         try {
           const data = res.data
           var obj = JSON.parse(data);
-  
+
           that.setData({
             generate: obj.rc,
           })
-  
+
+          console.log(obj)
           console.log(obj.rc)
 
         } catch (e) {
-          wx.showToast({
-            title: '远程服务器异常',
-            icon: 'none',
-            duration: 1000,
-          }),
+          console.log(e)
+
           that.setData({
             img: '',
             imgFile: '',
             generate: '',
           })
+
+          wx.showToast({
+            title: '远程服务器异常',
+            icon: 'none',
+            duration: 1000,
+          })
         }
       },
+
+      fail(res) {
+        console.log(res)
+      },
+
       complete(res) {
         that.setData({
           block: 'none'
@@ -117,4 +127,57 @@ Page({
       }
     })
   },
+
+  showImage: function () {
+    this.setData({
+      showimg: 'block'
+    })
+  },
+
+  hideImage: function () {
+    this.setData({
+      showimg: 'none'
+    })
+  },
+
+  downImage: function () {
+    var that = this
+
+    wx.showModal({
+      title: '保存图片',
+      content: '保存图片到系统相册',
+      success(res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+
+          var timestamp = new Date().getTime();
+          var aa = wx.getFileSystemManager();
+          aa.writeFile({
+            filePath: wx.env.USER_DATA_PATH + '/' + timestamp + '.png',
+            data: that.data.generate,
+            encoding: 'base64',
+            success: res => {
+              wx.saveImageToPhotosAlbum({
+                filePath: wx.env.USER_DATA_PATH + '/' + timestamp + '.png',
+                success: function (res) {
+                  wx.showToast({
+                    title: '保存成功',
+                  })
+                },
+                fail: function (err) {
+                  console.log(err)
+                }
+              })
+              console.log(res)
+            },
+            fail: err => {
+              console.log(err)
+            }
+          })
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  }
 })

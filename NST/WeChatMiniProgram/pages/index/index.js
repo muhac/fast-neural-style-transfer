9ced37b28984b2d79ec01acf5f797ad20919c059
Muhan Li <limuhan@msn.com>
2020-11-20 05:07:16 +0800

    微信小程序
    v2.1.2

--- NST/WeChatMiniProgram/pages/index/index.js
+++ NST/WeChatMiniProgram/pages/index/index.js
@@ -1,352 +1,254 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
-    img: 
-      '',
-    imgFile: 
-      '',
-    generate: 
-      '',
-    block: 
-      'none',
-    showimg: 
-      'none',
-    loss: 
-      'none',
-    style: 
-      1,
-    style_pos: [
-      220, 
-      5
-    ],
+    img: "",
+    imgFile: "",
+    generate: "",
+    block: "none",
+    showimg: "none",
+    loss: "none",
+    style: 1,
+    style_pos: [220, 5],
   },
 
   //事件处理函数
-  bindViewTap: 
-    function () {
-      wx.navigateTo({
-        url: 
-          '../logs/logs'
-      })
-    },
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: "../logs/logs",
+    });
+  },
 
-  onLoad: 
-    function () {
-      this.setData({
-        //imgFile: 123,
-      })
-      console.log('start')
-    },
+  onLoad: function () {
+    this.setData({
+      //imgFile: 123,
+    });
+    console.log("start");
+  },
 
-  chooseImage: 
-    function () {
-      var that = this;
-      wx.chooseImage({
-        count: 
-          1,
-        sizeType: [
-          'compressed'
-        ],
-        sourceType: [
-          'album', 
-          'camera'
-        ],
-        success: 
-          function (res) {
-            let base64 = wx.getFileSystemManager()
-                          .readFileSync(
-                            res.tempFilePaths[0], 
-                            'base64'
-                            )
-            // console.log(base64)
+  chooseImage: function () {
+    var that = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ["compressed"],
+      sourceType: ["album", "camera"],
+      success: function (res) {
+        let base64 = wx
+          .getFileSystemManager()
+          .readFileSync(res.tempFilePaths[0], "base64");
+        // console.log(base64)
 
-            that.setData({
-              imgFile: 
-                res.tempFilePaths[0],
-              img: 
-                base64,
-              generate: 
-                ''
-            })
-          }
-      })
-    },
+        that.setData({
+          imgFile: res.tempFilePaths[0],
+          img: base64,
+          generate: "",
+        });
+      },
+    });
+  },
 
-  chooseStyle:
-    function (e) {
-      var id = e.currentTarget
-                .dataset['index']
-      var y = parseInt((id - 1) / 3)
-      var x = parseInt(id - 1 - y * 3)
-      this.setData({
-        style: 
-          id,
-        style_pos: [
-          220 + 204 * x, 
-          5 + 204 * y
-        ]
-      })
-    },
+  chooseStyle: function (e) {
+    var id = e.currentTarget.dataset["index"];
+    var y = parseInt((id - 1) / 3);
+    var x = parseInt(id - 1 - y * 3);
+    this.setData({
+      style: id,
+      style_pos: [220 + 204 * x, 5 + 204 * y],
+    });
+  },
 
-  deleteImage: 
-    function () {
-      this.setData({
-        imgFile: 
-          ''
-      })
-    },
+  deleteImage: function () {
+    this.setData({
+      imgFile: "",
+    });
+  },
 
-  uploadImage: 
-    function (e) {
-      var that = this
+  uploadImage: function (e) {
+    var that = this;
 
-      this.setData({
-        block: 
-          'block'
-      })
+    this.setData({
+      block: "block",
+    });
 
-      wx.showToast({
-        title: 
-          '由于个人服务器资源有限\n\
-           请稍等几秒返回计算结果',
-        icon: 
-          'none',
-        duration: 
-          6000,
-        mask: 
-          true,
-      })
+    wx.showToast({
+      title: "由于个人服务器资源有限\n\
+           请稍等几秒返回计算结果",
+      icon: "none",
+      duration: 6000,
+      mask: true,
+    });
 
-      console.log('transfer')
+    console.log("transfer");
 
-      wx.uploadFile({
-        url: 
-          'https://www.bugstop.site/nst/',
-        filePath: 
-          that.data.imgFile,
-        name: 
-          'file',
-        formData: {
-          'style': 
-            that.data.style,
-          'img': 
-            that.data.img
-        },
+    wx.uploadFile({
+      url: "https://www.bugstop.site/nst/",
+      filePath: that.data.imgFile,
+      name: "file",
+      formData: {
+        style: that.data.style,
+        img: that.data.img,
+      },
 
-        success(res) {
-          try {
-            const data = res.data
-            var obj = JSON.parse(data);
+      success(res) {
+        try {
+          const data = res.data;
+          var obj = JSON.parse(data);
 
-            if (obj.rc == "fail") {
-              wx.showToast({
-                title: 
-                  '图片校验失败',
-                icon: 
-                  'none',
-                duration: 
-                  1000,
-              })
-            } else {
-              that.setData({
-                generate: obj.rc,
-              })
-
-              console.log(obj)
-              console.log(obj.rc)
-            }
-          } catch (e) {
-            console.log(e)
-
-            that.setData({
-              img: 
-                '',
-              imgFile: 
-                '',
-              generate: 
-                '',
-            })
-
+          if (obj.rc == "fail") {
             wx.showToast({
-              title: 
-                '远程服务器异常',
-              icon: 
-                'none',
-              duration: 
-                1000,
-            })
+              title: "图片校验失败",
+              icon: "none",
+              duration: 1000,
+            });
+          } else {
+            that.setData({
+              generate: obj.rc,
+            });
+
+            console.log(obj);
+            console.log(obj.rc);
           }
-        },
+        } catch (e) {
+          console.log(e);
 
-        fail(res) {
-          console.log(res)
-        },
-
-        complete(res) {
           that.setData({
-            block: 
-              'none'
-          })
-          console.log('done')
+            img: "",
+            imgFile: "",
+            generate: "",
+          });
+
+          wx.showToast({
+            title: "远程服务器异常",
+            icon: "none",
+            duration: 1000,
+          });
         }
-      })
-    },
+      },
 
-  showImage: 
-    function () {
-      console.log("show")
-      this.setData({
-        showimg: 
-          'block'
-      })
-    },
+      fail(res) {
+        console.log(res);
+      },
 
-  hideImage: 
-    function () {
-      console.log("hide")
-      this.setData({
-        showimg: 
-          'none'
-      })
-    },
+      complete(res) {
+        that.setData({
+          block: "none",
+        });
+        console.log("done");
+      },
+    });
+  },
 
-  hideLoss: 
-    function () {
-      this.setData({
-        showimg: 
-          'block',
-        loss: 
-          'none'
-      })
-    },
+  showImage: function () {
+    console.log("show");
+    this.setData({
+      showimg: "block",
+    });
+  },
 
-  downImage: 
-    function () {
-      console.log("down")
-      var that = this
+  hideImage: function () {
+    console.log("hide");
+    this.setData({
+      showimg: "none",
+    });
+  },
 
-      wx.showActionSheet({
-        itemList: [
-          '查看训练曲线', 
-          '保存图片到本地'
-        ],
+  hideLoss: function () {
+    this.setData({
+      showimg: "block",
+      loss: "none",
+    });
+  },
 
-        success: 
-          function (res) {
-            console.log(res.tapIndex)
+  downImage: function () {
+    console.log("down");
+    var that = this;
 
-            if (res.tapIndex == 0) {
-              that.setData({
-                loss: 
-                  'block',
-                showimg: 
-                  'none'
-              })
-            }
+    wx.showActionSheet({
+      itemList: ["查看训练曲线", "保存图片到本地"],
 
-            if (res.tapIndex == 1) {
-              var timestamp = new Date().getTime();
-              var aa = wx.getFileSystemManager();
-              aa.writeFile({
-                filePath: 
-                  wx.env.USER_DATA_PATH 
-                  + '/' 
-                  + timestamp 
-                  + '.png',
-                data: 
-                  that.data.generate,
-                encoding: 
-                  'base64',
-                success: res => {
-                  wx.saveImageToPhotosAlbum({
-                    filePath: 
-                      wx.env.USER_DATA_PATH 
-                      + '/' 
-                      + timestamp 
-                      + '.png',
-                    success: 
-                      function (res) {
-                        wx.showToast({
-                          title: '保存成功',
-                        })
-                      },
-                    fail: 
-                      function (err) {
-                        console.log(err)
-                      }
-                  })
-                  console.log(res)
+      success: function (res) {
+        console.log(res.tapIndex);
+
+        if (res.tapIndex == 0) {
+          that.setData({
+            loss: "block",
+            showimg: "none",
+          });
+        }
+
+        if (res.tapIndex == 1) {
+          var timestamp = new Date().getTime();
+          var aa = wx.getFileSystemManager();
+          aa.writeFile({
+            filePath: wx.env.USER_DATA_PATH + "/" + timestamp + ".png",
+            data: that.data.generate,
+            encoding: "base64",
+            success: (res) => {
+              wx.saveImageToPhotosAlbum({
+                filePath: wx.env.USER_DATA_PATH + "/" + timestamp + ".png",
+                success: function (res) {
+                  wx.showToast({
+                    title: "保存成功",
+                  });
                 },
-                fail: err => {
-                  console.log(err)
-                }
-              })
-            }
-          },
-
-        fail: 
-          function (res) {
-            console.log(res.errMsg)
-          }
-      })
-
-      var flag = false
-      if (flag) {
-
-        wx.showModal({
-          title: 
-            '保存图片',
-          content: 
-            '保存图片到系统相册',
-          success(res) {
-            if (res.confirm) {
-              console.log('用户点击确定')
-
-              var timestamp = new Date().getTime();
-              var aa = wx.getFileSystemManager();
-              aa.writeFile({
-                filePath: 
-                  wx.env.USER_DATA_PATH 
-                  + '/' 
-                  + timestamp 
-                  + '.png',
-                data: 
-                  that.data.generate,
-                encoding: 
-                  'base64',
-                success: res => {
-                  wx.saveImageToPhotosAlbum({
-                    filePath: 
-                      wx.env.USER_DATA_PATH 
-                      + '/' 
-                      + timestamp 
-                      + '.png',
-                    success: 
-                      function (res) {
-                        wx.showToast({
-                          title: '保存成功',
-                        })
-                      },
-                    fail: 
-                      function (err) {
-                        console.log(err)
-                      }
-                  })
-                  console.log(res)
+                fail: function (err) {
+                  console.log(err);
                 },
-                fail: err => {
-                  console.log(err)
-                }
-              })
-            } else if (res.cancel) {
-              console.log('用户点击取消')
-            }
-          }
-        })
+              });
+              console.log(res);
+            },
+            fail: (err) => {
+              console.log(err);
+            },
+          });
+        }
+      },
 
-      }
+      fail: function (res) {
+        console.log(res.errMsg);
+      },
+    });
+
+    var flag = false;
+    if (flag) {
+      wx.showModal({
+        title: "保存图片",
+        content: "保存图片到系统相册",
+        success(res) {
+          if (res.confirm) {
+            console.log("用户点击确定");
+
+            var timestamp = new Date().getTime();
+            var aa = wx.getFileSystemManager();
+            aa.writeFile({
+              filePath: wx.env.USER_DATA_PATH + "/" + timestamp + ".png",
+              data: that.data.generate,
+              encoding: "base64",
+              success: (res) => {
+                wx.saveImageToPhotosAlbum({
+                  filePath: wx.env.USER_DATA_PATH + "/" + timestamp + ".png",
+                  success: function (res) {
+                    wx.showToast({
+                      title: "保存成功",
+                    });
+                  },
+                  fail: function (err) {
+                    console.log(err);
+                  },
+                });
+                console.log(res);
+              },
+              fail: (err) => {
+                console.log(err);
+              },
+            });
+          } else if (res.cancel) {
+            console.log("用户点击取消");
+          }
+        },
+      });
     }
-})
+  },
+});
